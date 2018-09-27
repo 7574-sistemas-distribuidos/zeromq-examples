4120efe885394c9f68f329c47de40025513ce5ec
Ezequiel Torres <efeyuk@medallia.com>
2018-09-27 12:29:50 -0300

    Fixes in build.sh base-images

--- base-images/build.sh
+++ base-images/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f base.dockerfile -t grpc-base:0.0.1 .
-docker build -f golang-base.dockerfile -t grpc-golang-base:0.0.1 .
+docker build -f base.dockerfile -t zeromq-base:0.0.1 .
+docker build -f python-base.dockerfile -t zeromq-python-base:0.0.1 .
