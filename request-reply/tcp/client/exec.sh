9010f18555318955f0427bf7013d24bf226f8d61
Ezequiel Torres <efeyuk@medallia.com>
2018-09-27 07:26:38 -0300

    Changes to make the TCP server work again

--- request-reply/tcp/client/exec.sh
+++ request-reply/tcp/client/exec.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network $(docker network ls | grep request-reply | awk '{print $1}') --volume $(pwd):/tmp -it zeromq-python-base:0.0.1 bash 
+docker run --rm --network $(docker network ls | grep tcp_default | awk '{print $1}') --volume $(pwd):/tmp -it zeromq-python-base:0.0.1 bash 
--- request-reply/tcp/stop.sh
+++ request-reply/tcp/stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker-compose stop
 docker-compose down
-
-# docker rm -f $(docker ps -a | grep request-reply_ | awk '{print $1'})
